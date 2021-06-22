694f0e91880b58479f952710fc936cd62cafdd07
lukyanovpavel <pavel.lukyanovv@gmail.com>
2021-06-22 13:05:37 +0300

    refactor: code

--- app/src/main/java/com/pavellukyanov/cinematic/ui/popularmovie/PopularMovieFragment.kt
+++ app/src/main/java/com/pavellukyanov/cinematic/ui/popularmovie/PopularMovieFragment.kt
@@ -2,19 +2,13 @@ package com.pavellukyanov.cinematic.ui.popularmovie
 
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
 import androidx.fragment.app.viewModels
-import androidx.navigation.fragment.findNavController
 import androidx.paging.PagingData
-import androidx.recyclerview.widget.GridLayoutManager
 import com.pavellukyanov.cinematic.R
 import com.pavellukyanov.cinematic.databinding.FragmentPopularMovieBinding
 import com.pavellukyanov.cinematic.domain.models.Movie
-import com.pavellukyanov.cinematic.ui.adapters.MovieItemClickListener
 import com.pavellukyanov.cinematic.ui.adapters.MovieListAdapter
 import com.pavellukyanov.cinematic.ui.base.BaseFragment
-import com.pavellukyanov.cinematic.ui.main.MainFragmentDirections
-import com.pavellukyanov.cinematic.utils.Constants
 import com.pavellukyanov.cinematic.utils.MovieComparator
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -23,7 +17,7 @@ class PopularMovieFragment : BaseFragment<PagingData<Movie>>(R.layout.fragment_p
     private var _binding: FragmentPopularMovieBinding? = null
     private val binding get() = _binding!!
     private val viewModel: PopularMovieViewModel by viewModels()
-    private val popAdapter by lazy { MovieListAdapter(MovieComparator, movieItemClickListener) }
+    private val movieListAdapter by lazy { MovieListAdapter(MovieComparator, movieItemClickListener) }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -33,7 +27,7 @@ class PopularMovieFragment : BaseFragment<PagingData<Movie>>(R.layout.fragment_p
     }
 
     private fun initRecycler() {
-        binding.bindAdapter(requireContext(), popAdapter)
+        binding.bindAdapter(requireContext(), movieListAdapter)
     }
 
     private fun subscribeViewModel() {
@@ -42,7 +36,7 @@ class PopularMovieFragment : BaseFragment<PagingData<Movie>>(R.layout.fragment_p
 
     override fun handleSuccessStateMovies(data: PagingData<Movie>) {
         super.handleSuccessStateMovies(data)
-        popAdapter.submitData(lifecycle, data)
+        movieListAdapter.submitData(lifecycle, data)
     }
 
     override fun onDestroy() {
